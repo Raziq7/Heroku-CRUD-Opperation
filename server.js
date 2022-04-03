99e4efdcc233e5b7bb1e7b66b0883e49a2b55ab1
Raziq7 <raziqsur@gmail.com>
2022-04-04 00:16:15 +0530

    make it better 1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import config from './config';
 
 const { PORT } = config;
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log(
     'Express server listening on port %d in %s mode',
     this.address().port,
