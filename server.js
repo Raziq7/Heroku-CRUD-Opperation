b00d1073dfcc75112f85aee96a46efbcac243b8c
Raziq7 <raziqsur@gmail.com>
2022-04-04 00:09:41 +0530

    make it better

--- server.js
+++ server.js
@@ -3,4 +3,11 @@ import config from './config';
 
 const { PORT } = config;
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
+});
+// app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
